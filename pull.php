c1bec158224e289ab68663c97464da41ac5552b1
zad0xsis <zad0xsis@mailer.linux.solucion.net>
2011-10-06 20:24:46 +0200

    commit

--- pull.php
+++ pull.php
@@ -7,7 +7,7 @@ if($_GET['pass'] == sha1($password)) {
 	if($payload->commits[0]->branch === 'master') {
 
 		fwrite(fopen('./bitbucket.txt', 'a'), "(".date("d/m/Y")." - ".date("h:m:s").") NEW COMMIT => ".$payload->commits[0]->message);
-		//echo shell_exec('git pull');
+		echo shell_exec('git pull');
 	} else {
 		fwrite(fopen('./bitbucket.txt', 'a'), "(".date("d/m/Y")." - ".date("h:m:s").") UNKNOWN BRAND => ".$payload->commits[0]->branch."\n");
 

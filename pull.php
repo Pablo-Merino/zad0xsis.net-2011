10562d0e3d2c4bd864fa2b349b3ef0bf482cc405
Pablo Merino <pablo.perso1995@gmail.com>
2011-10-06 19:49:21 +0200

    ffffffffffffggggg

--- pull.php
+++ pull.php
@@ -7,15 +7,15 @@ $password = "ULTRAmegaHIPERsuperdifficultpassword18365385937267--..,.--,-..,-";
 if($_GET['pass'] == sha1($password)) {
 	if($payload->commits[0]->branch === 'master') {
 
-		fwrite(fopen('./extra/logs/bitbucket.txt', 'a'), "(".date("d/m/Y")." - ".date("h:m:s").") NEW COMMIT => ".$payload->commits[0]->message);
+		fwrite(fopen('./bitbucket.txt', 'a'), "(".date("d/m/Y")." - ".date("h:m:s").") NEW COMMIT => ".$payload->commits[0]->message);
 		//echo shell_exec('git pull');
 	} else {
-		fwrite(fopen('./extra/logs/bitbucket.txt', 'a'), "(".date("d/m/Y")." - ".date("h:m:s").") UNKNOWN BRAND => ".$payload->commits[0]->branch."\n");
+		fwrite(fopen('./bitbucket.txt', 'a'), "(".date("d/m/Y")." - ".date("h:m:s").") UNKNOWN BRAND => ".$payload->commits[0]->branch."\n");
 
 
 	}
 	
 } else {
-	fwrite(fopen('./extra/logs/bitbucket.txt', 'a'), "(".date("d/m/Y")." - ".date("h:m:s").") WRONG GET KEY\n");
+	fwrite(fopen('./bitbucket.txt', 'a'), "(".date("d/m/Y")." - ".date("h:m:s").") WRONG GET KEY\n");
 }
 ?>

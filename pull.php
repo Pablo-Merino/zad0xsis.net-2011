3a785fdc63518b4aed90e3a42cbd46fb6a41dae3
Pablo Merino <pablo.perso1995@gmail.com>
2011-10-06 19:36:30 +0200

    test

---
+++ pull.php
@@ -0,0 +1,24 @@
+<?php
+
+$payload = json_decode(stripslashes($_POST['payload']));
+
+$password = "ULTRAmegaHIPERsuperdifficultpassword18365385937267--..,.--,-..,-";
+
+if($_GET['pass'] == sha1($password)) {
+	if($payload->commits[0]->branch === 'master') {
+
+		fwrite(fopen('./extra/logs/bitbucket.txt', 'a'), "(".date("d/m/Y")." - ".date("h:m:s").") NEW COMMIT => ".$payload->commits[0]->message);
+		//echo shell_exec('git pull');
+	} else {
+		fwrite(fopen('./extra/logs/bitbucket.txt', 'a'), "(".date("d/m/Y")." - ".date("h:m:s").") UNKNOWN BRAND => ".$payload->commits[0]->branch."\n");
+
+
+	}
+	
+} else {
+	fwrite(fopen('./extra/logs/bitbucket.txt', 'a'), "(".date("d/m/Y")." - ".date("h:m:s").") WRONG GET KEY\n");
+
+}
+?>
+
+?>
